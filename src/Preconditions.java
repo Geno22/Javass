4d196c3ece818586ca52317ef836ce1d94a162a5
thismad <mathis.dehez@gmail.com>
2019-02-19 08:51:47 +0100

    premier push

---
+++ src/Preconditions.java
@@ -0,0 +1,30 @@
+/*
+ * Author: Mathis Dehez
+ * Date: 18 févr. 2019
+*/
+
+public class Preconditions {
+	
+	private Preconditions() {}
+	
+	/**
+	 * throw IAE if argument is false
+	 * @param b
+	 * @throws IllegalArgumentException
+	 */
+	public static void checkArgument(boolean b) throws IllegalArgumentException{
+		if (!b) throw new IllegalArgumentException();	
+	}
+	
+	/**
+	 * check if an index is in the good range and throw IOOBE if not, otherwise return index
+	 * @param index
+	 * @param size
+	 * @return index
+	 * @throws IndexOutOfBoundsException
+	 */
+	public static int checkIndex(int index, int size) throws IndexOutOfBoundsException{
+		if (index >=size || index <0) throw new IndexOutOfBoundsException();
+		else return index;
+	}
+}
